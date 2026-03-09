33fae4c488b210a00eccfc32bb969a10bbdbbcec
Shaun the Sheep <118677376+Prasanta-ray@users.noreply.github.com>
2026-03-10 00:59:09 +0530

    Extract notice/routine data and simplify HomeScreen
    Move large static datasets out of home_screen.dart into new files (notice_data.dart and routine_data.dart) to separate concerns and reduce file size. Simplify _HomeScreenState by removing the Scaffold, AppBar, NavigationBar and drawer from this file and returning only the selected screen wrapped in SafeArea; the parent Scaffold is expected to provide the app-level chrome. This refactor improves maintainability and keeps UI layout responsibilities clearer.

---
+++ notice_data.dart
@@ -0,0 +1,23 @@
+final List<Map<String, String>> noticeData = [
+  {
+    "title": "Registrations Now Open | CITK MUN 2026",
+    "date": "Jan 10, 2026",
+    "sender": "CITK MODEL UNITED NATIONS",
+    "body":
+        "Greetings from the CITK Model United Nations Secretariat! We are delighted to announce that registrations are now officially open for CITK MUN 2026. Dates: 23rd-25th Jan 2026. Venue: CIT Kokrajhar. Outstanding performers will be honored with Certificates, Trophies, and Exciting Gifts."
+  },
+  {
+    "title": "Power Supply Restored",
+    "date": "Jan 10, 2026",
+    "sender": "Electrical Team CITK",
+    "body":
+        "Power supply has been restored to the campus following the scheduled interruption for urgent repair at 132 kV GSS, Adabari. Thank you for your patience and support."
+  },
+  {
+    "title": "DD Robocon India 2026 Outreach",
+    "date": "Jan 8, 2026",
+    "sender": "Dean Alumni and External Relations",
+    "body":
+        "STPI in association with Prasar Bharti and IIT Delhi is organizing DD Robocon India 2026. The winner will represent India at the ABU Robocon in Hong Kong. Pre-registration deadline: 9 January 2026."
+  }
+];
---
+++ routine_data.dart
@@ -0,0 +1,660 @@
+final Map<String, dynamic> routineData = {
+  // ==========================================
+  // MONDAY
+  // ==========================================
+  "MONDAY": {
+    "M.Tech_M.Des_II": {
+      "CSE": {
+        "room": "PG Class Room",
+        "slots": [
+          "",
+          "MCS218:BKC",
+          "MCS201:AN",
+          "MCS202:HKK",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      },
+      "WRH": {
+        "room": "PG ROOM",
+        "slots": [
+          "",
+          "MCE201:SMJ",
+          "MCE202:SMJ",
+          "MCE2101:AVP",
+          "Lunch Break",
+          "MCE2108:MMG",
+          "",
+          ""
+        ]
+      },
+      "GET": {
+        "room": "PG Class Room",
+        "slots": [
+          "",
+          "MGE214",
+          "",
+          "MGE202: RKR",
+          "Lunch Break",
+          "",
+          "Energy Lab II",
+          ""
+        ]
+      },
+      "FET": {
+        "room": "PG Class Room",
+        "slots": [
+          "",
+          "",
+          "MFE203: ABD",
+          "MFE2122: SKR",
+          "Lunch Break",
+          "MFE2111: SSM",
+          "SEMINAR-I: MFE291: RDK/SBR",
+          ""
+        ]
+      },
+      "DESIGN": {
+        "room": "",
+        "slots": [
+          "",
+          "LAB:MMD 211:SDN",
+          "",
+          "LAB:MMD:212:NF",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      }
+    },
+    "M.Tech_M.Des_IV": {
+      "CSE": {
+        "room": "PG Class Room",
+        "slots": ["", "", "Project Lab", "", "Lunch Break", "", "", ""]
+      },
+      "WRH": {
+        "room": "",
+        "slots": [
+          "",
+          "",
+          "Project",
+          "",
+          "Lunch Break",
+          "Project",
+          "Project",
+          ""
+        ]
+      },
+      "GET": {
+        "room": "",
+        "slots": ["", "", "Project", "", "Lunch Break", "Project", "", ""]
+      },
+      "FET": {
+        "room": "",
+        "slots": ["", "", "", "", "Lunch Break", "", "", ""]
+      },
+      "DESIGN": {
+        "room": "",
+        "slots": ["", "", "", "", "Lunch Break", "", "", ""]
+      }
+    },
+    "Diploma_Sem_II": {
+      "ET_CT_FP": {
+        "room": "231",
+        "slots": [
+          "DME201: HDM",
+          "DPH201:MBB",
+          "DMA201-AAB",
+          "DCH201(ANK/ARM)",
+          "Lunch Break",
+          "DCSE201:PKS",
+          "DME 271: HDM/SJI/MNG/SBS",
+          ""
+        ]
+      },
+      "CO_CI_AM": {
+        "room": "232",
+        "slots": [
+          "",
+          "DCSE201:APS",
+          "DPH201:MB",
+          "DMA201-JKK",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      },
+      "AM": {
+        "room": "",
+        "slots": [
+          "",
+          "",
+          "DMD201:DKB",
+          "LAB:DMD271:DKB",
+          "Lunch Break",
+          "DMD202:BKS",
+          "LAB:DMD272:BKS",
+          ""
+        ]
+      }
+    },
+    "Diploma_Sem_IV": {
+      "ET": {
+        "room": "125",
+        "slots": [
+          "DEC 401 ATP",
+          "DEC404 PKM",
+          "DEC471 ATP",
+          "",
+          "Lunch Break",
+          "DHS401:TNK/MDB (231)",
+          "DEC415 KKP",
+          ""
+        ]
+      },
+      "CE": {
+        "room": "119",
+        "slots": [
+          "",
+          "",
+          "",
+          "DCE404:NLD",
+          "Lunch Break",
+          "DHS401:TNK/MDB (231)",
+          "DCE403:JKB",
+          "DCE401: OMP"
+        ]
+      },
+      "FP": {
+        "room": "235",
+        "slots": [
+          "",
+          "DME 401: HDM",
+          "DIE405:BRB",
+          "DFE402: ANI",
+          "Lunch Break",
+          "DHS401:TNK/MDB (231)",
+          "Lab: DFE473 (L): ANI/PKN",
+          ""
+        ]
+      },
+      "CO": {
+        "room": "",
+        "slots": [
+          "",
+          "DCS402:AKB (304)",
+          "DCS405:MTK (304)",
+          "DCS401:TNS (304)",
+          "Lunch Break",
+          "DIE403: DPS",
+          "DCS471 Lab:TNS (CSE Lab4)",
+          ""
+        ]
+      },
+      "CI": {
+        "room": "303",
+        "slots": [
+          "DEE401(DTS)",
+          "DIE402:JTD",
+          "DIE401: DPD",
+          "",
+          "Lunch Break",
+          "DIE403: DPS",
+          "DIE474: AKR, BPC",
+          ""
+        ]
+      },
+      "AM": {
+        "room": "",
+        "slots": [
+          "",
+          "DAMT404:SDN",
+          "LAB:DAMT474:SDN",
+          "",
+          "Lunch Break",
+          "LAB:DAMT472:NF",
+          "",
+          ""
+        ]
+      }
+    },
+    "Diploma_Sem_VI": {
+      "ET": {
+        "room": "125",
+        "slots": [
+          "",
+          "DECE695 KKP",
+          "DECE604: JDHB",
+          "DECE601 BBP",
+          "Lunch Break",
+          "DHSS602: KSB",
+          "",
+          ""
+        ]
+      },
+      "CE": {
+        "room": "119",
+        "slots": [
+          "DCE603:NLD",
+          "DCE612:AVP",
+          "DCE602:JKB",
+          "",
+          "Lunch Break",
+          "",
+          "DCE679",
+          ""
+        ]
+      },
+      "FP": {
+        "room": "235",
+        "slots": [
+          "",
+          "DFET691:PROJECT",
+          "",
+          "",
+          "Lunch Break",
+          "DFET612: ABD",
+          "DFET691:PROJECT",
+          ""
+        ]
+      },
+      "CO": {
+        "room": "",
+        "slots": [
+          "",
+          "",
+          "DCSE612:PPS (236)",
+          "DCSE613:BKC (236)",
+          "Lunch Break",
+          "DHSS601: PBC (304)",
+          "",
+          ""
+        ]
+      },
+      "CI": {
+        "room": "303",
+        "slots": [
+          "",
+          "Project : DIE691",
+          "",
+          "DEE611(DTS)",
+          "Lunch Break",
+          "DHSS601: PBC (304)",
+          "DIE601: SWK",
+          ""
+        ]
+      },
+      "AM": {
+        "room": "",
+        "slots": [
+          "",
+          "",
+          "",
+          "",
+          "Lunch Break",
+          "DHSS601: PBC (304)",
+          "DAMT601:SBR",
+          "LAB:DAMT671:SBR"
+        ]
+      }
+    },
+    "Degree_Sem_II": {
+      "ECE_IE": {
+        "room": "126",
+        "slots": [
+          "UMA201-SDB",
+          "UHS201: BHB",
+          "UCSE201:DKR",
+          "UME 201 : SRB",
+          "Lunch Break",
+          "",
+          "UHS202:JDHB",
+          ""
+        ]
+      },
+      "CSE_G2": {
+        "room": "G2",
+        "slots": [
+          "",
+          "",
+          "UMA201-SMD",
+          "UHS202:JDHB",
+          "Lunch Break",
+          "UCSE201:AKB",
+          "UCSE271 Lab:AKB (CSE Lab2)",
+          ""
+        ]
+      },
+      "CE_FET_G1": {
+        "room": "G1",
+        "slots": [
+          "UHS201: BHB",
+          "UME 201 : SRB",
+          "UCH201(GND/PJK)",
+          "UMA201-SDB/SMD",
+          "Lunch Break",
+          "UCSE201:DKR",
+          "",
+          ""
+        ]
+      },
+      "MCD": {
+        "room": "",
+        "slots": [
+          "UMD 203:ABP",
+          "",
+          "",
+          "LAB:UMD 273:ABP",
+          "Lunch Break",
+          "",
+          "STUDIO:UMD 294:ABP",
+          ""
+        ]
+      }
+    },
+    "Degree_Sem_IV": {
+      "ECE": {
+        "room": "123",
+        "slots": [
+          "",
+          "UEC401 NMD",
+          "UHS401:KSB",
+          "",
+          "Lunch Break",
+          "UECE491 NMD",
+          "",
+          ""
+        ]
+      },
+      "IE": {
+        "room": "227",
+        "slots": [
+          "",
+          "UEE401(RJD)",
+          "UIE402: JTD",
+          "UIE401: KDS",
+          "Lunch Break",
+          "UIE471: KDS, APN",
+          "",
+          ""
+        ]
+      },
+      "CSE_A": {
+        "room": "",
+        "slots": [
+          "",
+          "",
+          "UMA401-GCR (229)",
+          "UCS401:RJP (230)",
+          "Lunch Break",
+          "UCS403:MTK (230)",
+          "UHS471Lab: TNK/BHB/MLB",
+          ""
+        ]
+      },
+      "CSE_B": {
+        "room": "",
+        "slots": [
+          "",
+          "UCS401:RJP (229)",
+          "UMA401-GCR (229)",
+          "UHS401:KSB (229)",
+          "Lunch Break",
+          "UCS471 Lab:RJP (CSE Lab1)",
+          "UCS403:MTK (229)",
+          ""
+        ]
+      },
+      "CE": {
+        "room": "121",
+        "slots": [
+          "",
+          "",
+          "",
+          "UHS401:PBC",
+          "Lunch Break",
+          "UCE402:ABD",
+          "UCE404:AKD",
+          "UCE401:YCO"
+        ]
+      },
+      "FET": {
+        "room": "233",
+        "slots": [
+          "",
+          "UFE412: ANI",
+          "UHS401:PBC",
+          "UFE403: AVM",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      },
+      "MCD": {
+        "room": "",
+        "slots": [
+          "",
+          "UMCD 401:BKS",
+          "LAB:UMCD 471:BKS",
+          "",
+          "Lunch Break",
+          "STUDIO:UMCD 491:SDN",
+          "LAB:UMCD472:NF",
+          ""
+        ]
+      }
+    },
+    "Degree_Sem_VI": {
+      "ECE": {
+        "room": "124",
+        "slots": [
+          "",
+          "UECE601 AGM",
+          "UECE602 BBP",
+          "UECE616 HDC/RJC (R-123/124)",
+          "Lunch Break",
+          "UECE671 AGM",
+          "",
+          ""
+        ]
+      },
+      "IE": {
+        "room": "301",
+        "slots": [
+          "",
+          "UIE601: RKR",
+          "UIE611: KDS",
+          "UIE602: DPD",
+          "Lunch Break",
+          "UIE612: AKR",
+          "",
+          ""
+        ]
+      },
+      "CSE_A": {
+        "room": "",
+        "slots": [
+          "",
+          "UHSS601: MDB (228)",
+          "UCSE602:PSB (228)",
+          "",
+          "Lunch Break",
+          "UCSE601:RNM (228)",
+          "",
+          ""
+        ]
+      },
+      "CSE_B": {
+        "room": "",
+        "slots": [
+          "",
+          "UCSE671 Lab:RNM (CSE Lab2)",
+          "UHSS601: MDB (228)",
+          "UCSE602:PSB (229)",
+          "Lunch Break",
+          "UCSE601:RNM (229)",
+          "",
+          ""
+        ]
+      },
+      "CE": {
+        "room": "122",
+        "slots": [
+          "",
+          "UCE672:BRS,SMP(A)/UCE671:AKD,AJD/KHS(B)",
+          "UCE603:AKD",
+          "UCE602:RMH",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      },
+      "FET": {
+        "room": "234",
+        "slots": [
+          "",
+          "UFET601:SKR",
+          "UIE604: RKR",
+          "UFET612: MAG",
+          "Lunch Break",
+          "",
+          "Lab: UIE674(L): RKR/ANB",
+          ""
+        ]
+      },
+      "MCD": {
+        "room": "",
+        "slots": [
+          "",
+          "",
+          "",
+          "UMCD 604:DKB",
+          "Lunch Break",
+          "LAB:UMCD 674:DKB",
+          "",
+          ""
+        ]
+      }
+    },
+    "Degree_Sem_VIII": {
+      "ECE": {
+        "room": "124",
+        "slots": [
+          "",
+          "UECE895",
+          "UECE811A SNB",
+          "UECE812A ADM",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      },
+      "IE": {
+        "room": "301/227",
+        "slots": [
+          "",
+          "Project Lab",
+          "UIE811: TKM",
+          "UIE818/ UIE819 (301)/ UIE820 (227)",
+          "Lunch Break",
+          "Project Lab",
+          "",
+          ""
+        ]
+      },
+      "CSE_A": {
+        "room": "",
+        "slots": [
+          "",
+          "Project Lab",
+          "Project Lab",
+          "UCSE812:BKC (228)",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      },
+      "CSE_B": {
+        "room": "",
+        "slots": [
+          "",
+          "Project Lab",
+          "Project Lab",
+          "UCSE814:PPS (230)",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      },
+      "CE": {
+        "room": "122",
+        "slots": [
+          "UCE802:BRS",
+          "UCE801:OMP",
+          "UCE816:RMH",
+          "",
+          "Lunch Break",
+          "UCE891",
+          "",
+          ""
+        ]
+      },
+      "FET": {
+        "room": "234",
+        "slots": [
+          "",
+          "UFET891: MAJOR PROJECT-II",
+          "",
+          "",
+          "Lunch Break",
+          "UFET801: MAG",
+          "UFET811: SBR",
+          ""
+        ]
+      },
+      "MCD": {
+        "room": "",
+        "slots": [
+          "",
+          "",
+          "",
+          "Elective-V:UMCD 811 :ABP",
+          "Lunch Break",
+          "",
+          "",
+          ""
+        ]
+      }
+    }
+  },
+  // ==========================================
+  // TUESDAY (Copy of Monday so it's not empty)
+  // ==========================================
+  "TUESDAY": {
+    "Diploma_Sem_IV": {
+      "CO": {
+        "room": "",
+        "slots": [
+          "DCS402:AKB",
+          "DCS405:MTK",
+          "Lab",
+          "Lunch Break",
+          "DIE403: DPS",
+          "",
+          "",
+          ""
+        ]
+      }
+    },
+    // ... Copy other Monday items here if needed
+  }
+};
